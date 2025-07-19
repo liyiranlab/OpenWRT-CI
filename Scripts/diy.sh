ab1bd214f64f405da2ca587ffdce1d1efa6ea722
liyiranlab <58244219+liyiranlab@users.noreply.github.com>
2025-07-19 20:38:06 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -137,8 +137,8 @@ sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 #echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 #echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 #echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-theme-argon=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-argon-config=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-argon=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
